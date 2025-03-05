34df0e199e83e4517b5f4a4efbbca02936351675
AadarshKhanna <133052894+AadarshKhanna@users.noreply.github.com>
2025-03-05 15:17:47 +0530

    Update doctorRoutes.js

--- backend/routes/doctorRoutes.js
+++ backend/routes/doctorRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { getDoctors, addDoctor, updateDoctor, deleteDoctor } = require("../controllers/doctorController");
 
+const router = express.Router();
+
 router.get("/", getDoctors);
 router.post("/", addDoctor);
 router.put("/:id", updateDoctor);
 router.delete("/:id", deleteDoctor);
 
 module.exports = router;
-
-
